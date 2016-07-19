db0d3bfad176f59f8bd887029f13b5cc210f03dd
eight <eight04@gmail.com>
2016-07-19 08:41:59 +0800

    Add more test

--- test/test.js
+++ test/test.js
@@ -13,3 +13,8 @@ console.assert(
     bbsReader("\x1b[1m\x1b[33mA\x1b[m\x1b[33mB").html
     == "<div class='line'><span class='f3 b0 l w1'>A</span><span class='f3 b0 w1'>B</span></div>"
 );
+
+console.assert(
+    bbsReader("\x1b[1mA\x1b[33mB\x1b[;33;40mC").html
+    == "<div class='line'><span class='f7 b0 l w1'>A</span><span class='f3 b0 l w1'>B</span><span class='f3 b0 w1'>C</span></div>"
+)
