8bbc81a8621d623d8c1742f2b6f35885c0f062c6
eight <eight04@gmail.com>
2016-07-19 08:28:26 +0800

    Add more test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 var bbsReader = require("../bbs-reader");
 
-    text = "\xa2\x1b[m\x1b[m\x67",
-    result = bbsReader(text);
-    
 function getTextContent(html) {
     return html.replace(/<.+?>/g, "");
 }
 
-console.assert(getTextContent(result.html) == "\xa2\x67\xa2\x67");
+console.assert(
+    getTextContent(bbsReader("\xa2\x1b[m\x1b[m\x67").html)
+    == "\xa2\x67\xa2\x67"
+);
+
+console.assert(
+    bbsReader("\x1b[1m\x1b[33mA\x1b[m\x1b[33mB").html
+    == "<div class='line'><span class='f3 b0 l w1'>A</span><span class='f3 b0 w1'>B</span></div>"
+);
